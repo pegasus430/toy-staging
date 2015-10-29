80371c31ec016de801aa72442fc0f7eab9dd169c
softcomet <lukastore688@gmail.com>
2015-10-29 07:37:03 +0100

    micropost controller created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029040959) do
+ActiveRecord::Schema.define(version: 20151029063625) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
